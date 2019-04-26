a57a5b04f944cf30c3d64b20500b25b8f8af56a6
NaveenkumarP0018 <p.naveen0018@gmail.com>
2019-04-26 12:36:04 +0530

    commit

---
+++ typescript/basic.ts
@@ -0,0 +1,47 @@
+// //boolean type
+// let isdone:boolean=false;
+// console.log(isdone)
+// let decimal: number = 6;
+// let hex: number = 0xf00d;
+// let binary: number = 0b1010;
+// let octal: number = 0o744;
+// console.log(hex)
+// //string
+// let fullName: string = `Bob Bobbington`;
+// let age: number = 37;
+// let sentence: string = `Hello, my name is ${ fullName }.
+
+// I'll be ${ age + 1 } years old next month.`;
+// console.log(sentence)
+
+// let color:string="blue"
+// color="123"
+// console.log(color)
+// // function f() {
+// //     var a = 12;
+// //     return function g() {
+// //         var b = a + 1;
+// //         return b;
+// //     }
+// // }
+
+// // var g = f();
+// // console.log(g());
+// function f() {
+//     var a = 1;
+
+//     a = 2;
+//     var b = g();
+//     a = 3;
+
+//     return b;
+
+//     function g() {
+//         return a;
+//     }
+// }
+
+// console.log(f()); // returns '2'
+
+// var str:any= '1' 
+// str=1
