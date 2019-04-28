c83effc23dc822527818e41d5c3226c275103bf6
NaveenkumarP0018 <p.naveen0018@gmail.com>
2019-04-28 09:43:45 +0530

    commit

--- typescript/basic.js
+++ typescript/basic.js
@@ -1,40 +1,80 @@
-// //boolean type
-// let isdone:boolean=false;
-// console.log(isdone)
-// let decimal: number = 6;
-// let hex: number = 0xf00d;
-// let binary: number = 0b1010;
-// let octal: number = 0o744;
-// console.log(hex)
-// //string
-// let fullName: string = `Bob Bobbington`;
-// let age: number = 37;
-// let sentence: string = `Hello, my name is ${ fullName }.
-// I'll be ${ age + 1 } years old next month.`;
-// console.log(sentence)
-// let color:string="blue"
-// color="123"
-// console.log(color)
-// // function f() {
-// //     var a = 12;
-// //     return function g() {
-// //         var b = a + 1;
-// //         return b;
-// //     }
-// // }
-// // var g = f();
-// // console.log(g());
-// function f() {
-//     var a = 1;
-//     a = 2;
-//     var b = g();
-//     a = 3;
-//     return b;
-//     function g() {
-//         return a;
-//     }
-// }
-// console.log(f()); // returns '2'
+//boolean type
+var isdone = false;
+console.log(isdone);
+console.log("===================================================");
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+console.log(hex);
+console.log("===================================================");
+//string
+var fullName = "Bob Bobbington";
+var age = 37;
+var sentence = "Hello, my name is " + fullName + ".\n\nI'll be " + (age + 1) + " years old next month.";
+console.log(sentence);
+console.log("===================================================");
+var color = "blue";
+color = "123";
+console.log(color);
+console.log("===================================================");
+function f() {
+    var a = 12;
+    return function g() {
+        var b = a + 1;
+        return b;
+    };
+}
+var g = f();
+console.log(g());
+console.log("===================================================");
+function f1() {
+    var a = 1;
+    a = 2;
+    var b = g();
+    a = 3;
+    return b;
+    function g() {
+        return a;
+    }
+}
+console.log(f1()); // returns '2'
+console.log("===================================================");
 var str = '1';
-var str2 = str; //str is now of type number 
-console.log(str);
+str = 1;
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.walk = function (distance) {
+        document.write("hi,my name is" + this.name + " and iam walking " + (distance + 1) + "meters");
+    };
+    return Animal;
+}());
+var a1 = new Animal("danger");
+a1.walk(10);
+console.log("===================================================");
+var Employee = /** @class */ (function () {
+    function Employee(eID, name) {
+        this.empID = eID;
+        this.fullName = name;
+    }
+    Employee.prototype.toString = function () {
+        console.log("EMP ID of " + name);
+    };
+    return Employee;
+}());
+var Customer = /** @class */ (function () {
+    function Customer(cID, name) {
+        this.custID = cID;
+        this.fullName = name;
+    }
+    Customer.prototype.toString = function () {
+        console.log("Customer ID of " + name);
+    };
+    return Customer;
+}());
+var employee = new Employee("E001", "Kunal");
+console.log(employee);
+var customer = new Customer("C001", "");
+console.log(customer);
--- typescript/basic.ts
+++ typescript/basic.ts
@@ -1,47 +1,103 @@
-// //boolean type
-// let isdone:boolean=false;
-// console.log(isdone)
-// let decimal: number = 6;
-// let hex: number = 0xf00d;
-// let binary: number = 0b1010;
-// let octal: number = 0o744;
-// console.log(hex)
-// //string
-// let fullName: string = `Bob Bobbington`;
-// let age: number = 37;
-// let sentence: string = `Hello, my name is ${ fullName }.
+//boolean type
+let isdone:boolean=false;
+console.log(isdone)
+console.log("===================================================")
 
-// I'll be ${ age + 1 } years old next month.`;
-// console.log(sentence)
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+console.log(hex)
+console.log("===================================================")
 
-// let color:string="blue"
-// color="123"
-// console.log(color)
-// // function f() {
-// //     var a = 12;
-// //     return function g() {
-// //         var b = a + 1;
-// //         return b;
-// //     }
-// // }
+//string
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${ fullName }.
 
-// // var g = f();
-// // console.log(g());
-// function f() {
-//     var a = 1;
+I'll be ${ age + 1 } years old next month.`;
+console.log(sentence);
+console.log("===================================================")
+let color:string="blue"
+color="123"
+console.log(color)
+console.log("===================================================")
 
-//     a = 2;
-//     var b = g();
-//     a = 3;
+function f() {
+    var a = 12;
+    return function g() {
+        var b = a + 1;
+        return b;
+    }
+}
 
-//     return b;
+var g = f();
+console.log(g());
+console.log("===================================================")
 
-//     function g() {
-//         return a;
-//     }
-// }
+function f1() {
+    var a = 1;
 
-// console.log(f()); // returns '2'
+    a = 2;
+    var b = g();
+    a = 3;
 
-// var str:any= '1' 
-// str=1
+    return b;
+
+    function g() {
+        return a;
+    }
+}
+
+console.log(f1()); // returns '2'
+console.log("===================================================")
+
+
+var str:any= '1' 
+str=1
+class Animal{
+    private name:string;
+
+    constructor(name){
+        this.name=name;
+
+    }
+    walk(distance:number){
+        document.write("hi,my name is" + this.name + " and iam walking " +(distance + 1)+"meters");
+    }
+}
+let a1=new Animal("danger");
+a1.walk(10);
+console.log("===================================================")
+
+interface Person {
+    fullName: string;
+    toString();
+   }
+class Employee implements Person {
+    empID: string;
+    fullName: string;
+    constructor (eID: string, name: string) {
+     this.empID = eID;
+     this.fullName = name;
+    }
+    toString() {
+     console.log(`EMP ID of ${name}`);
+    }
+   }
+   class Customer implements Person {
+    custID: string;
+    fullName: string;
+    constructor (cID: string, name: string) {
+     this.custID = cID;
+     this.fullName = name;
+    }
+    toString() {
+     console.log(`Customer ID of ${name}`);
+    }
+   }
+
+   let  employee: Person = new Employee("E001", "Kunal");
+   console.log(employee)
+let customer: Person = new Customer("C001", "");
+console.log(customer)
