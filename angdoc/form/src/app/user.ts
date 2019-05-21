716e098fce05315b6f88ee99bf1516ffc0cb837a
NaveenkumarP0018 <p.naveen0018@gmail.com>
2019-05-21 20:32:11 +0530

    commit

---
+++ angdoc/form/src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public name:string,
+        public emial:string,
+        public phone:number,
+        public topic:string,
+        public TimePreference:string,
+        public subscribe:boolean 
+
+    ){}
+}
